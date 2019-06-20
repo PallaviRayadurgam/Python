cdccca263b0c1ce5b3c3b9bfc888d3dea2bd2ae4
PallaviRayadurgam <41808972+PallaviRayadurgam@users.noreply.github.com>
2019-06-19 23:49:14 -0500

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,11 +1,11 @@
-#  Task 
-# Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+""" Task 
+Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
 
-# You don't need to perform any rounding or formatting operations.
+You don't need to perform any rounding or formatting operations.
 
-# Input Format
+Input Format
 
-# The first line contains the first integer, . The second line contains the second integer, .
+The first line contains the first integer, . The second line contains the second integer, ."""
 
 
 if __name__ == '__main__':
